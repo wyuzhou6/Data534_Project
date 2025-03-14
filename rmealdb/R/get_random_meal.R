aa6db752f0320a73ff603cbbfb7fc70ef132c3a4
PPSHOU <qnbwyb@163.com>
2025-03-13 20:10:07 -0700

    code update for get random meal and search meal

--- rmealdb/R/get_random_meal.R
+++ rmealdb/R/get_random_meal.R
@@ -5,9 +5,13 @@
 #' @return A dataframe containing details of a randomly selected meal.
 #' @examples
 #' get_random_meal()
+#' @export
 get_random_meal <- function() {
   url <- "https://www.themealdb.com/api/json/v1/1/random.php"
-  response <- httr::GET(url)
-  data <- jsonlite::fromJSON(httr::content(response, "text", encoding = "UTF-8"))
-  return(data$meals)
+
+  response <- httr2::request(url) %>%
+    httr2::req_perform() %>%
+    httr2::resp_body_json()
+
+  return(response$meals)
 }
