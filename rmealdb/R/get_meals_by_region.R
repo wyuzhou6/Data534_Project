8cb5f95410972b7de697e807ec688a0a258c6a4d
PPSHOU <qnbwyb@163.com>
2025-03-13 21:02:43 -0700

    edit file

--- rmealdb/R/get_meals_by_region.R
+++ rmealdb/R/get_meals_by_region.R
@@ -6,15 +6,19 @@
 #' @return A dataframe containing meal names and their IDs.
 #' @examples
 #' get_meals_by_region("Italian")
+#' @export
 get_meals_by_region <- function(region) {
   url <- paste0("https://www.themealdb.com/api/json/v1/1/filter.php?a=", region)
-  response <- httr::GET(url)
-  data <- jsonlite::fromJSON(httr::content(response, "text", encoding = "UTF-8"))
-  
-  if (is.null(data$meals)) {
+
+  response <- httr2::request(url) %>%
+    httr2::req_perform() %>%
+    httr2::resp_body_json()
+
+  if (is.null(response$meals)) {
     message("Invalid region or no meals found.")
     return(NULL)
   }
-  
-  return(data$meals)
+
+  return(response$meals)
+}
 }
