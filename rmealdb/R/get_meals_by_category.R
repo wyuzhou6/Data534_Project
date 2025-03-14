c4febfee590b49587fdb94745d27a07f3b76b4da
PPSHOU <qnbwyb@163.com>
2025-03-13 20:03:02 -0700

    update code for getmeal by category

--- rmealdb/R/get_meals_by_category.R
+++ rmealdb/R/get_meals_by_category.R
@@ -5,17 +5,20 @@
 #' @param category A character string specifying the category.
 #' @return A dataframe containing meal names and their IDs.
 #' @examples
-#' @export
 #' get_meals_by_category("Seafood")
+#' @export
 get_meals_by_category <- function(category) {
   url <- paste0("https://www.themealdb.com/api/json/v1/1/filter.php?c=", category)
-  response <- httr::GET(url)
-  data <- jsonlite::fromJSON(httr::content(response, "text", encoding = "UTF-8"))
-  
-  if (is.null(data$meals)) {
+
+  response <- httr2::request(url) %>%
+    httr2::req_perform() %>%
+    httr2::resp_body_json()
+
+  if (is.null(response$meals)) {
     message("Invalid category or no meals found.")
     return(NULL)
   }
-  
-  return(data$meals)
+
+  return(response$meals)
 }
+
