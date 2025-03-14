6ba3ca696b44fba4b0f1683d8398855ed9fa9c87
PPSHOU <qnbwyb@163.com>
2025-03-13 20:13:06 -0700

    update code

--- rmealdb/R/utils.R
+++ rmealdb/R/utils.R
@@ -2,12 +2,19 @@
 #'
 #' Checks if the API response is valid and extracts data.
 #'
-#' @param response The HTTP response object from httr::GET()
+#' @param response The HTTP response object from httr2.
 #' @return A parsed JSON object or NULL if the response is invalid.
+#' @examples
+#' handle_api_response(response)
+#' @export
 handle_api_response <- function(response) {
-  if (httr::status_code(response) != 200) {
-    message("API request failed. Status code: ", httr::status_code(response))
+  # Check if the HTTP response status is not successful (status code 200)
+  if (httr2::resp_status(response) != 200) {
+    # Display a message indicating the failure with the status code
+    message("API request failed. Status code: ", httr2::resp_status(response))
+    # Return NULL to indicate failure in retrieving data
     return(NULL)
   }
-  jsonlite::fromJSON(httr::content(response, "text", encoding = "UTF-8"))
+  # Parse and return the JSON content from the response if successful
+  httr2::resp_body_json(response)
 }
